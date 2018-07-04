58d177ede4dcc5ecea3df2347bc694d62dcde9e1
dingxiao <dingxiao88@163.com>
2018-07-04 17:58:10 +0800

    添加markdown前端显示功能

--- Django_Profolio/Django_Profolio/views.py
+++ Django_Profolio/Django_Profolio/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from gallery.models import Gallery
+from . import biying
 
 
 def home(request):
+
+
+    # g = biying.get_one_photo()
+    # url_photo=g.get_one_photo()
 	gallerys = Gallery.objects
 	# return render(request, 'home.html', {'gallerys': gallerys})
 
-	return render(request, 'index.html',  {'gallerys': gallerys})
+	pic1 = biying.get_one_photo(0)
+	pic2 = biying.get_one_photo(1)
+	pic3 = biying.get_one_photo(2)
+
+	return render(request, 'index.html',  {'gallerys': gallerys, 'url_photo_1':pic1, 'url_photo_2':pic2, 'url_photo_3':pic3})
--- Django_Profolio/blog/views.py
+++ Django_Profolio/blog/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
@@ -9,4 +10,5 @@ def blog_page(request):
 
 def blog_text(request,blog_id):
 	blog=get_object_or_404(Blog,pk=blog_id)
+	blog.text = markdown.markdown(blog.text,extensions=['markdown.extensions.extra','markdown.extensions.codehilite','markdown.extensions.toc',])
 	return render(request, 'blog_text.html',{'blog':blog})
